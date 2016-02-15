078b81a8f3d150fa1e5d88b29cba0b2a001a2dd9
Stella Gao <stella.gao.89@gmail.com>
2016-02-15 16:23:49 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+/*Configuration Settings*/
+
+/*MySQL Settings*/
+/*Database name*/
+define('DB_NAME', 'web_traffic');
+
+/*Database user*/
+define('DB_USER', 'root');
+
+/*Database user password*/
+define('DB_PASSWORD', '');
+
+/* Chances are you won't need to change the following three
+settings. Only do so if you know what you're doing.*/
+
+/*Database host*/
+define('DB_HOST', 'localhost:3307');
+
+/*Database character set*/
+define('DB_CHARSET', 'utf8');
+
+
+
+
+?>
